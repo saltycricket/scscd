022ba8629ecdec72c201600ef31c20e4d78ecd86
Salty Cricket <saltycricket1@gmail.com>
2025-09-16 17:28:03 -0400

    Build release GUI tool and check for errors during packaging

--- bin/build-release.bat
+++ bin/build-release.bat
@@ -6,23 +6,38 @@ SET VER=%1
 rm -rf release
 mkdir release
 
+dotnet publish .\scscd-gui\scscd-gui.csproj ^
+       -c Release ^
+       -r win-x64 ^
+       --self-contained false ^
+       -p:PublishSingleFile=true
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 cp -r res release/scscd-%VER%-og
+if %errorlevel% neq 0 exit /b %errorlevel%
 cp build/og/scscd.dll release/scscd-%VER%-og/F4SE/Plugins/scscd.dll
-cp scscd-gui/bin/Release/net8.0-windows/scscd-gui.exe release/scscd-%VER%-og/scscd-config.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
+cp scscd-gui/bin/Release/net8.0-windows/win-x64/publish/scscd-gui.exe release/scscd-%VER%-og/F4SE/Plugins/scscd/scscd-config.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
 rm release/scscd-%VER%-og.7z
 cd release
 cd scscd-%VER%-og
 7z a -r ../scscd-%VER%-og.7z .
+if %errorlevel% neq 0 exit /b %errorlevel%
 cd ..
 cd ..
 
 cp -r res release/scscd-%VER%-ng
+if %errorlevel% neq 0 exit /b %errorlevel%
 cp build/ng/scscd.dll release/scscd-%VER%-ng/F4SE/Plugins/scscd.dll
-cp scscd-gui/bin/Release/net8.0-windows/scscd-gui.exe release/scscd-%VER%-ng/scscd-config.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
+cp scscd-gui/bin/Release/net8.0-windows/win-x64/publish/scscd-gui.exe release/scscd-%VER%-ng/F4SE/Plugins/scscd/scscd-config.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
 rm release/scscd-%VER%-ng.7z
 cd release
 cd scscd-%VER%-ng
 7z a -r ../scscd-%VER%-ng.7z .
+if %errorlevel% neq 0 exit /b %errorlevel%
 cd ..
 cd ..
 
